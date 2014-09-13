65d9067ada33e8b2f317049acc34573659f428ba
Kevin Michel <kmichel.info@gmail.com>
2014-09-13 21:06:19 +0200

    Add version number to the package

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 
 from distutils.core import setup
 
+__version__ = 'unknown'
+with open('po_localization/version.py') as version_file:
+    exec(version_file.read())
+
 setup(
     name='po_localization',
     packages=['po_localization'],
-    version='0.1.1',
+    version=__version__,
     description='Localize Django applications without compiling .po files',
     author='Kevin Michel',
     author_email='kmichel.info@gmail.com',
     url='https://github.com/kmichel/po-localization',
-    download_url='https://github.com/kmichel/po-localization/archive/v0.1.1.tar.gz',
+    download_url='https://github.com/kmichel/po-localization/archive/v{}.tar.gz'.format(__version__),
     keywords=['django', 'localization'],
     classifiers=[
         'Development Status :: 3 - Alpha',
